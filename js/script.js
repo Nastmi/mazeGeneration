d24334ea69dc29354739bc818a0d7380ac6e4248
Nastmi <natan.semrl@gmail.com>
2019-12-12 21:27:58 +0100

    Walls are tracked, but not removed.

--- js/script.js
+++ js/script.js
@@ -9,19 +9,42 @@ function initializeCanvas(){
     scaleX = mainCanvas.clientWidth/1920;
     scaleY = mainCanvas.clientHeight/749;
     context = mainCanvas.getContext("2d"); 
+    //drawGrid();
     generateMaze();
+}
+function drawGrid(gridArray){
+    context.strokeStyle="#FFFFFF";
+    for(let i=0;i<gridArray.length;i++){
+        for(let j=0;j<gridArray[i].length;j++){
+            for(let k=0;k<gridArray[i][j].length;k++){
+                if(gridArray[i][j].charAt(k) == "1"){ 
+                    context.beginPath();
+                    switch(k){
+                        case 0:context.moveTo(100+100*j,100+100*i);context.lineTo(100+100*j+100,100+100*i);break;
+                        case 1:context.moveTo(100+100*j+100,100+100*i);context.lineTo(100+100*j+100,100+100*i+100);break;
+                        case 2:context.moveTo(100+100*j+100,100+100*i+100);context.lineTo(100+100*j,100+100*i+100);break;
+                        case 3:context.moveTo(100+100*j,100+100*i+100);context.lineTo(100+100*j,100+100*i);break;
+                    }
+                    context.stroke();
+                }
+            }
+        }
+    }
 }
 
 
 function generateMaze(){
+    let gridLength = 6;
+    let gridArray = Array(6).fill().map(() => Array(6).fill("1111"));
     let visited = [];
     let stack = [];
-    let currentCell = Math.floor(Math.random()*37);/*getCell(Math.floor(Math.random()*36).toString);*/
+    drawGrid(gridArray);
+    let currentCell = Math.floor(Math.random()*36+1);/*getCell(Math.floor(Math.random()*36).toString);*/
     while(true){
         visited.push(currentCell);
         stack.push(currentCell);
-        let sur = returnSurroundingCells(currentCell);
-        let checked = [];
+        let sur = returnSurroundingCells(currentCell, gridLength);
+        let checked = []; 
         while(true){
             let index = Math.floor(Math.random()*4);
             if(checked.length == 4){
@@ -29,7 +52,7 @@ function generateMaze(){
                 stack.pop();
                 break;
             }
-            if(sur[index]<0 || sur[index]>36){
+            if(sur[index]<1 || sur[index]>36 || (index == 0 && sur[index]%gridLength == 0) || (index == 1 && sur[index]%gridLength == 1)){
                 if(!checked.includes(index))
                     checked.push(index);
                 continue;
@@ -37,28 +60,33 @@ function generateMaze(){
             else{
                 if(visited.includes(sur[index])){
                     if(!checked.includes(index)){
-                        console.log("pushed index");
                         checked.push(index);
                     }
                     continue;
                 }
                 else{
                     currentCell = sur[index];
-                    context.beginPath();
-                    if(index == 0){
-                        context.moveTo(parseInt(currentCell.toString.split("")[0])*10,parseInt(currentCell.toString.split("")[1])*10);
-                        context.lineTo(parseInt(currentCell.toString.split("")[0])*10+100,parseInt(currentCell.toString.split("")[1])*10);
-                    }
-                    context.stroke();
+                    console.log(Math.floor(currentCell/6)+" "+currentCell%5);
+                    console.log(gridArray[Math.floor(currentCell/5)][currentCell%5]);
+                    
+                   /* switch(index){
+                        case 0:gridArray[Math.floor(currentCell/6)-1][currentCell%6] = replaceCharAt(gridArray[Math.floor(currentCell/6)][currentCell%6],index+2,"0");break;
+                        case 1:gridArray[Math.floor(currentCell/6)][currentCell%6-1] = replaceCharAt(gridArray[Math.floor(currentCell/6)][currentCell%6],index+2,"0");break;
+                        case 2:gridArray[Math.floor(currentCell/6)+1][currentCell%6] = replaceCharAt(gridArray[Math.floor(currentCell/6)][currentCell%6],index-2,"0");break;
+                        case 3:gridArray[Math.floor(currentCell/6)][currentCell%6+1] = replaceCharAt(gridArray[Math.floor(currentCell/6)][currentCell%6],index-2,"0");break;
+                    }*/
                     break;
                 }
             }
         }
-       // console.log("am out now");
         if(visited.length >= 36)
             break;
     }
+    drawGrid(gridArray);
 }
-function returnSurroundingCells(cellNum){
-    return [cellNum-1,cellNum+1,cellNum-10,cellNum+10];
+function returnSurroundingCells(cellNum, gridLength){
+    return [cellNum-1,cellNum+1,cellNum-gridLength,cellNum+gridLength];
+}
+function replaceCharAt(string, index, replace){
+    return string.substring(0,index)+replace+string.substring(index+1,string.length);
 }
